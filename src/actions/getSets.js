0820aee973760732938e1a38753645c38002318d
Gurtaj <gurtaj@gmail.com>
2018-04-04 17:54:25 +0100

    edit getSets.js so that actions only deal with sending the received data from the get reponses. edit Sets.js so that all data can be copied into the store which is copied and manipulated into the required form

--- src/actions/getSets.js
+++ src/actions/getSets.js
@@ -6,46 +6,72 @@ export default function getSets(){
     return (dispatch) => {
         return axios.get(baseURL+"/api/sets/")  //get sets
         .then(response => {
-            let dataArr = []; //create array to hold sets-data and extra data to be retrieved below
-            for (let i=0; i<response.data.objects.length; i++) { //for each set
-                dataArr.push(response.data.objects[i]);
-                if (response.data.objects[i].image_urls.length>0) { //if there are any images for the current set
-                    axios.get(baseURL+response.data.objects[i].image_urls[0]) //get the first image
-                    .then(response1 => {
-                        dataArr[i].image = response1.data.url; //add that image to the data array
+            dispatch({
+                type: "GET_SETS",
+                data: response.data.objects
+            })
+            return response;
+        })
+        .then(response => {
+            response.data.objects.map(object => {
+                if (object.image_urls.length>0) {
+                    return axios.get(baseURL+object.image_urls[0])  //get first image for each set which has image(s)
+                    .then(res => {
+                        dispatch({
+                            type: "GET_IMAGE",
+                            data: res.data
+                        })
                     })
-                } else {
-                    dataArr[i].image = "http://www.contactyellowpages.com/images/no_image.jpg";
+                    .catch(error => {
+                        dispatch({
+                            type: "IMAGE_ERROR",
+                            payload: error
+                        })  
+                    })
                 }
-                dataArr[i].episodes = []; // create array within data array for storing episode data
-                if (response.data.objects[i].items.length>0) { //if the set has items (equivalent to episodes and dividers)
-                    for (let j=0; j<response.data.objects[i].items.length; j++) {
-                        if (response.data.objects[i].items[j].content_type === "episode") { //for each item which is an episode
-                            axios.get(baseURL+response.data.objects[i].items[j].content_url) //get the episode data
-                            .then(response2 => {
-                                if (response2.data.image_urls.length>0){ //if there are any image api urls provoded
-                                    axios.get(baseURL+response2.data.image_urls[0]) //make a call to the image api url
-                                    .then(response3 => {
-                                        response2.data.image = response3.data.url; //add the direct image url to the episode data object
-                                        dataArr[i].episodes.push(response2.data); //add the episode data to the data array
+            })
+            return response;
+        })
+        .then(response => {
+            response.data.objects.map(object => {
+                if (object.items.length>0) {
+                    object.items.map(item => {
+                        if (item.content_type === "episode") {
+                            axios.get(baseURL+item.content_url)  //get each episode-item for each set which has items in it
+                            .then(res => {
+                                dispatch({
+                                    type: "GET_EPISODE",
+                                    data: res.data
+                                })
+                                return res;
+                            })
+                            .then(res => {
+                                if (res.data.image_urls.length>0){
+                                    axios.get(baseURL+res.data.image_urls[0])  //get first episode-image for each episode which has image(s)
+                                    .then(res1 => {
                                         dispatch({
-                                            type: "GET_SETS",
-                                            data: dataArr
+                                            type: "GET_EPISODE_IMAGE",
+                                            data: res1.data
                                         })
                                     })
-                                } else {
-                                    response2.data.image = "http://www.contactyellowpages.com/images/no_image.jpg";
-                                    dataArr[i].episodes.push(response2.data);
-                                    dispatch({
-                                        type: "GET_SETS",
-                                        data: dataArr
+                                    .catch(error => {
+                                        dispatch({
+                                            type: "EPISODE_IMAGE_ERROR",
+                                            payload: error
+                                        })   
                                     })
                                 }
                             })
+                            .catch(error => {
+                                dispatch({
+                                    type: "EPISODE_ERROR",
+                                    payload: error
+                                })   
+                            })
                         }
-                    }
+                    })
                 }
-            }
+            })
         })
         .catch(error => {
             dispatch({
@@ -57,3 +83,45 @@ export default function getSets(){
 }
 
 
+// .then(response => {
+//     let dataArr = []; //create array to hold sets-data and extra data to be retrieved below
+//     for (let i=0; i<response.data.objects.length; i++) { //for each set
+//         dataArr.push(response.data.objects[i]);
+//         if (response.data.objects[i].image_urls.length>0) { //if there are any images for the current set
+//             axios.get(baseURL+response.data.objects[i].image_urls[0]) //get the first image
+//             .then(response1 => {
+//                 dataArr[i].image = response1.data.url; //add that image to the data array
+//             })
+//         } else {
+//             dataArr[i].image = "http://www.contactyellowpages.com/images/no_image.jpg";
+//         }
+//         dataArr[i].episodes = []; // create array within data array for storing episode data
+//         if (response.data.objects[i].items.length>0) { //if the set has items (equivalent to episodes and dividers)
+//             for (let j=0; j<response.data.objects[i].items.length; j++) {
+//                 if (response.data.objects[i].items[j].content_type === "episode") { //for each item which is an episode
+//                     axios.get(baseURL+response.data.objects[i].items[j].content_url) //get the episode data
+//                     .then(response2 => {
+//                         if (response2.data.image_urls.length>0){ //if there are any image api urls provoded
+//                             axios.get(baseURL+response2.data.image_urls[0]) //make a call to the image api url
+//                             .then(response3 => {
+//                                 response2.data.image = response3.data.url; //add the direct image url to the episode data object
+//                                 dataArr[i].episodes.push(response2.data); //add the episode data to the data array
+//                                 dispatch({
+//                                     type: "GET_SETS",
+//                                     data: dataArr
+//                                 })
+//                             })
+//                         } else {
+//                             response2.data.image = "http://www.contactyellowpages.com/images/no_image.jpg";
+//                             dataArr[i].episodes.push(response2.data);
+//                             dispatch({
+//                                 type: "GET_SETS",
+//                                 data: dataArr
+//                             })
+//                         }
+//                     })
+//                 }
+//             }
+//         }
+//     }
+// })
--- src/reducers/sets.js
+++ src/reducers/sets.js
@@ -3,21 +3,110 @@ const initialState = {
                         data: []
                     };
 
+function addData(state=initialState, data) {
+    let newStateData = state.data.slice(0);
+    newStateData = data;
+    for (let i=0; i<newStateData.length; i++) {
+        newStateData[i].episodes = [];
+        newStateData[i].image = "http://www.contactyellowpages.com/images/no_image.jpg";
+    }
+    return {
+        ...state,
+        error: null,
+        data: newStateData
+    }
+}
+
+function addImage(state=initialState, image) {
+    let newStateData = state.data.slice(0);
+    for (let i=0; i<newStateData.length; i++) {
+        if (newStateData[i].image_urls.length>0) {
+            if (newStateData[i].self === image.content_url) {
+                newStateData[i].image = image.url;
+            }
+        } 
+    }
+    return {
+        ...state,
+        error: null,
+        data: newStateData
+    }
+}
+
+function addEpisode(state=initialState, episode) {
+    let newStateData = state.data.slice(0);
+    for (let i=0; i<newStateData.length; i++) {
+        if (newStateData[i].items.length>0) {
+            for (let j=0; j<newStateData[i].items.length; j++) {
+                if (newStateData[i].items[j].content_url === episode.self) {
+                    newStateData[i].episodes.push(episode);
+                }
+            }
+        } 
+    }
+    return {
+        ...state,
+        error: null,
+        data: newStateData
+    }
+}
+
+function addEpisodeImage(state=initialState, episodeImage) {
+    console.log(episodeImage)
+    let newStateData = state.data.slice(0);
+    for (let i=0; i<newStateData.length; i++) {
+        if(newStateData[i].episodes.length>0) {
+            for (let j=0; j<newStateData[i].episodes.length; j++) {
+                if(newStateData[i].episodes[j].image_urls[0] === episodeImage.self) {
+                    newStateData[i].episodes[j].image = episodeImage.url;
+                } else {
+                    newStateData[i].episodes[j].image = "http://www.contactyellowpages.com/images/no_image.jpg";
+                }
+            }
+        }
+    }
+    return {
+        ...state,
+        error: null,
+        data: newStateData
+    }
+}
+
 export default function(state = initialState, action) {
     switch (action.type) {
         case 'GET_SETS':
+            return addData(state, action.data);
+        case 'GET_IMAGE':
+            return addImage(state, action.data);
+        case 'GET_EPISODE':
+            return addEpisode(state, action.data);
+        case 'GET_EPISODE_IMAGE':
+            return addEpisodeImage(state, action.data);
+        case 'IMAGE_ERROR':
             return {
                 ...state,
-                error: null,
-                data: action.data
-            }
+                error: true,
+                data: []
+            };
+        case 'EPISODE_ERROR':
+            return {
+                ...state,
+                error: true,
+                data: []
+            };
+        case 'EPISODE_IMAGE_ERROR':
+            return {
+                ...state,
+                error: true,
+                data: []
+            };
         case 'SETS_ERROR':
             return {
                 ...state,
                 error: true,
                 data: []
-            }
+            };
         default:
-            return state
+            return state;
     }
 }
